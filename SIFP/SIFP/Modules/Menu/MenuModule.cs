52373b730edc4f2dcb94f6eec0dc3822fd861d48
zhu daokuan <15549519416@163.com>
2021-03-12 19:55:48 +0800

    添加漏了的文件

---
+++ SIFP/SIFP/Modules/Menu/MenuModule.cs
@@ -0,0 +1,26 @@
+﻿using Menu.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using SIFP.Core;
+
+namespace Menu
+{
+    public class MenuModule : IModule
+    {
+        private IRegionManager regionManager;
+        public MenuModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            regionManager.RequestNavigate(RegionNames.MenuRegion, "MenuView");
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<MenuView>("MenuView");
+        }
+    }
+}
---
+++ SIFP/SIFP/Modules/Menu/ViewModels/MenuViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Menu.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using SIFP.Core;
+using SIFP.Core.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu.ViewModels
+{
+    public class MenuViewModel : RegionViewModelBase
+    {
+        private string _message;
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+
+        public MenuViewModel(IRegionManager regionManager):base(regionManager)
+        {
+            Message = "MenuModule";
+        }
+    }
+}
