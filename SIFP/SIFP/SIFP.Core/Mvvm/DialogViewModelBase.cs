7e7fec681208432a3e245d825c187dac45de7d00
zhu daokuan <15549519416@163.com>
2021-03-15 19:58:25 +0800

    添加DialogViewModelBase作为DialogViewModel的父类

---
+++ SIFP/SIFP/SIFP.Core/Mvvm/DialogViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIFP.Core.Mvvm
+{
+
+    public class DialogViewModelBase : ViewModelBase, IDialogAware
+    {
+        public string Title { get; set; }
+
+        public event Action<IDialogResult> RequestClose;
+
+        public virtual bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public virtual void OnDialogClosed()
+        {
+            
+        }
+
+        public virtual void OnDialogOpened(IDialogParameters parameters)
+        {
+            
+        }
+    }
+}
