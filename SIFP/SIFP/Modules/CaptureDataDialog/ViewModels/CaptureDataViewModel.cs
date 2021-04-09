ab8785ad962a1423a279b44f96387e8184213137
zhu daokuan <15549519416@163.com>
2021-04-09 15:29:41 +0800

    CaptureData时continue表示1000帧

--- SIFP/SIFP/Modules/CaptureDataDialog/ViewModels/CaptureDataViewModel.cs
+++ SIFP/SIFP/Modules/CaptureDataDialog/ViewModels/CaptureDataViewModel.cs
@@ -44,9 +44,9 @@ namespace CaptureDataDialog.ViewModels
                 CaptureDataTypes.Add(new ComboBoxItemMode<CaptureDataTypeE> { Description = item.ToString(), SelectedModel = item, IsShow = Visibility.Visible });
             }
 
-            foreach (UInt32 item in new UInt32[] { 1, 5, 10, 20, 30, 50, 100, 200, 300, 0xFFFFFFFF })
+            foreach (UInt32 item in new UInt32[] { 1, 5, 10, 20, 30, 50, 100, 200, 300, 1000 })
             {
-                if (item == 0xffffffff)
+                if (item == 1000)
                     CaptureDataNums.Add(new ComboBoxItemMode<UInt32> { Description = "continue", SelectedModel = item, IsShow = Visibility.Visible });
                 else
                     CaptureDataNums.Add(new ComboBoxItemMode<UInt32> { Description = item.ToString(), SelectedModel = item, IsShow = Visibility.Visible });
