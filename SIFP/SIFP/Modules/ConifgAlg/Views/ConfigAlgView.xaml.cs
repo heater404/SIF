1bb8d8eec475a8e6a0ef28678dd8d2eac8786ad1
zhu daokuan <15549519416@163.com>
2021-05-11 17:48:37 +0800

    漏了的文件

---
+++ SIFP/SIFP/Modules/ConifgAlg/Views/ConfigAlgView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Events;
+using SIFP.Core.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConifgAlg.Views
+{
+    /// <summary>
+    /// Interaction logic for ViewA.xaml
+    /// </summary>
+    public partial class ConfigAlgView : UserControl
+    {
+        public ConfigAlgView(IEventAggregator eventAggregator)
+        {
+            InitializeComponent();
+            eventAggregator.GetEvent<ChangeDrawerRegionSizeEvent>().Subscribe(size =>
+            {
+                this.Height = Math.Max(size.Height - 80, 0);
+            }, true);
+        }
+    }
+}
---
+++ SIFP/SIFP/Services/CommunicationServices/InitConfigAlg.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+using Services.Interfaces;
+using SIFP.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class InitConfigAlg : IInitConfigAlg
+    {
+        public ConfigAlg Init()
+        {
+            try
+            {
+                string json = File.ReadAllText(@"Configs\ConfigAlg.json");
+                return JsonSerializer.Deserialize<ConfigAlg>(json);
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex.Message);
+            }
+
+            return null;
+        }
+    }
+}
