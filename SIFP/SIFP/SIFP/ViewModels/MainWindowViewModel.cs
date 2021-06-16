5ac9071bac8f56b91a5decc6b91b6d144ff647d8
zhu daokuan <15549519416@163.com>
2021-06-16 14:27:57 +0800

    剔除BG usercase and  修改版本号

--- SIFP/SIFP/SIFP/ViewModels/MainWindowViewModel.cs
+++ SIFP/SIFP/SIFP/ViewModels/MainWindowViewModel.cs
@@ -31,7 +31,7 @@ namespace SIFP.ViewModels
             set { SetProperty(ref title, value); }
         }
 
-        private string version = "1.05.210615";
+        private string version = "1.05.210616";
         public string Version
         {
             get { return version; }
