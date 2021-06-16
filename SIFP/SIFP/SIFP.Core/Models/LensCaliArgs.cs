a0e2e160dd1eb0cdba850867b22405312a701120
zhu daokuan <15549519416@163.com>
2021-06-16 17:19:42 +0800

    修改镜头参数的数据类型为带符号类型

--- SIFP/SIFP/SIFP.Core/Models/LensCaliArgs.cs
+++ SIFP/SIFP/SIFP.Core/Models/LensCaliArgs.cs
@@ -10,23 +10,23 @@ namespace SIFP.Core.Models
         [FieldOrder(1)]
         public UInt32 GainFx { get; set; } = 100;
         [FieldOrder(2)]
-        public UInt32 Fx { get; set; } = 528 * 100;
+        public Int32 Fx { get; set; } = 528 * 100;
         [FieldOrder(3)]
         public UInt32 GainFy { get; set; } = 100;
         [FieldOrder(4)]
-        public UInt32 Fy { get; set; } = 528 * 100;
+        public Int32 Fy { get; set; } = 528 * 100;
         [FieldOrder(5)]
         public UInt32 GainCx { get; set; } = 100;
         [FieldOrder(6)]
-        public UInt32 Cx { get; set; } = 320 * 100;
+        public Int32 Cx { get; set; } = 320 * 100;
         [FieldOrder(7)]
         public UInt32 GainCy { get; set; } = 100;
         [FieldOrder(8)]
-        public UInt32 Cy { get; set; } = 240 * 100;
+        public Int32 Cy { get; set; } = 240 * 100;
         [FieldOrder(9)]
         public UInt32 GainFovZoom { get; set; } = 100;
         [FieldOrder(10)]
-        public UInt32 FovZoom { get; set; } = 100;
+        public Int32 FovZoom { get; set; } = 100;
 
         public override string ToString()
         {
