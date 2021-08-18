240a876921bdefe60ce2e03eaca653c0db63cb8c
zhu daokuan <15549519416@163.com>
2021-08-18 15:31:22 +0800

    修改版本号

--- SIFP/SIFP/SIFP/ViewModels/MainWindowViewModel.cs
+++ SIFP/SIFP/SIFP/ViewModels/MainWindowViewModel.cs
@@ -31,7 +31,7 @@ namespace SIFP.ViewModels
             set { SetProperty(ref title, value); }
         }
 
-        private string version = "1.05.210813p1";
+        private string version = "1.05.210818";
         public string Version
         {
             get { return version; }
